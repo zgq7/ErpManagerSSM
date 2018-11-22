932b33c8e3b961c22e243f17008a360ef8add645
zgq7 <1140661106@qq.com>
2018-11-22 23:23:29 +0800

    11-22

--- src/main/Java/com/erp/controller/TestController.java
+++ src/main/Java/com/erp/controller/TestController.java
@@ -5,11 +5,9 @@ import com.erp.service.CustomerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
@@ -20,32 +18,34 @@ public class TestController {
     private CustomerService customerService;
 
     @RequestMapping(value = "/Login")
-    public String Login(Customer customer, HttpServletRequest request) {
-        System.out.println(request.toString());
-        System.out.println(customer.toString());
+    public String Login(HttpServletRequest request) {
+        Customer customer = new Customer();
+        //返回目标Jsp前缀
         String directJsp = null;
-        log.info("账号:{}，密码:{}",customer.getCardNo(),customer.getPassword());
+        customer.setPassword(request.getParameter("password"));
+        customer.setCardNo(request.getParameter("cardNo"));
+        log.info("账号:{}，密码:{}", customer.getCardNo(), customer.getPassword());
         try {
             String status = customerService.Login(customer);
-            log.info("返回登录状态：{}",status);
-            if (status.equals("true")){
+            log.info("返回登录状态：{}", status);
+            if (status.equals("true")) {
                 directJsp = "loginSuccess";
-            }else {
+            } else {
                 directJsp = "error";
             }
-        }catch (Exception e){
-            log.info("系统异常：",e);
+        } catch (Exception e) {
+            log.info("系统异常：", e);
         }
         return directJsp;
     }
 
     @RequestMapping("/getAllCustomer")
-    public String getAllCustomer(){
+    public String getAllCustomer() {
         try {
             List<Customer> c = customerService.CatchAllCustomer();
-            log.info("{}",c.toString());
-        }catch (Exception e){
-            log.info("系统异常：",e);
+            log.info("{}", c.toString());
+        } catch (Exception e) {
+            log.info("系统异常：", e);
         }
         return "CatchAllCustomer";
     }
